7cc68676f0b57cbc4bbe0176aba919fe5bce8d58
Philip Loibl <philip.loibl@student.tugraz.at>
2020-07-20 04:29:09 +0200

    fixed folder structure

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,26 @@ mod_version=$(cat info.json | jq -r '.version')
 
 archive_name="${mod_name}_${mod_version}.zip"
 
+tempdir=$(mktemp -d)
+
+echo $tempdir
+
+mkdir "${tempdir}/${mod_name}_${mod_version}"
+
+rsync -r --exclude '.git' . "${tempdir}/${mod_name}_${mod_version}"
+
+cwd=$(pwd)
+cd $tempdir
+
 mod_dir=""
 if [ -d "$FACTORIO_MOD_DIR" ]; then
-  # Control will enter here if $DIRECTORY exists.
   mod_dir=$FACTORIO_MOD_DIR
+else
+  mod_dir=$cwd
 fi
 
+zip -r "${mod_dir}/${archive_name}" "${mod_name}_${mod_version}"
 
+cd $cwd
 
-zip -r "${mod_dir}${archive_name}" ./* -x .git
+rm -R $tempdir
